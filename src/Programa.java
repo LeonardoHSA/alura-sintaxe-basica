9332d4a192a468e5d81f4c4e4202bee76825a954
Leonardo_Sanfins <leonardo.henrique@multiway.com.br>
2022-04-05 13:00:52 -0300

    Primeiro commit

---
+++ src/Programa.java
@@ -0,0 +1,6 @@
+
+public class Programa { // public class Programa é uma classe que demos o nome de Programa
+	public static void main(String[] args) { // public static void main é um método, e dentro dele que colocamos nosso código
+		System.out.println("ola mundo");
+	}
+}
